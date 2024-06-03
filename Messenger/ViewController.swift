b7ac4e8a89db6e69b2022ca29e43498c2127a791
Ken Hoang <hqhoang311@gmail.com>
2024-06-03 19:12:19 -0400

    initial commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Ken Hoang on 2024-06-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
